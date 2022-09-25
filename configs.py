73ef9bc1b2f37accee0d39f646c56fa0d16646dd
jmcwx <114340812+jmcwx@users.noreply.github.com>
2022-09-25 17:27:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,10 +4,10 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	API_ID = int(os.environ.get("API_ID", "19890227"))
+	API_HASH = os.environ.get("API_HASH", "ad2685e918be5a89c31e991448262e63")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5211965803:AAErvvVKh3XWfzhVEHqCashhS9B5I0piR9A") 
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", " filemini")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
